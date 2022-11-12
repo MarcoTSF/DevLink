ff902e530c87bb43991fc79b15ba81af1c6c7e5d
MarcoTSF <mtsf.salvador@gmail.com>
2022-11-11 23:57:06 -0300

    Criação rota Admin

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -4,10 +4,34 @@ import { auth } from '../services/firebaseConnection'
 import { onAuthStateChanged } from 'firebase/auth'
 
 import { Navigate } from 'react-router-dom'
+import { async } from '@firebase/util'
 
 export default function Private({ children }){
     const [loading, setLoading] = useState(true)
     const [signed, setSigned] = useState(false);
+
+    useEffect( () => {
+        async function checkLogin(){
+            const unsub = onAuthStateChanged(auth, () => {
+                if(user){
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email
+                    };
+
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+                    setLoading(false);
+                    setSigned(true);
+                } else{
+                    setLoading(false);
+                    setSigned(false);
+                }
+            })
+        }
+
+        checkLogin();
+
+    }, [])
 
     if(loading){
         return(
